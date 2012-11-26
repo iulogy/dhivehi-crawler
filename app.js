46e15c2d1f4968336ff61832b104986f1c42fd4e
iuthisaam <iuthisaam@isa0004.(none)>
2012-11-26 08:58:38 +0500

    added source argument

--- app.js
+++ app.js
@@ -2,13 +2,14 @@
 //process.stdout.write ('\u001B[2J\u001B[0;0f')
 
 var util = require('util');
-var arg = require('optimist');
-
+var arg = require('optimist').argv;
 var sites = require('./lib');
 
 var site = new sites.site();
-
-site.requestLimit = arg.limit +1 || 6
+if(arg.sites){
+	site.sources = typeof arg.sites == 'object' ? arg.sites : new Array(arg.sites);
+}
+site.requestLimit = arg.limit +1 || 6;
 site.on('error', function(err){
 	console.log(err);
 })
