7452b80e32402cbc0f349e01633e53be6d4641a7
iuthisaam <iuthisaam@isa0004.(none)>
2012-11-28 21:29:56 +0500

    fixed bug

--- crawler.js
+++ crawler.js
@@ -1,13 +1,16 @@
 var spawn = require('child_process').spawn;
 var arg = require('optimist').argv;
 
-setInterval(x, 1000 * 60 * arg.interval);
+setInterval(x, 1000 * 60 * (arg.interval || 2));
 
 function x (){
-	app = spawn('node', ['app.js']);
+	app = spawn('node', ['app.js', '--limit', arg.limit || 3]);
 	app.stdout.setEncoding('utf8');
 	app.stdout.on('data', function(data){
 		console.log(data);
 	})
+	app.on('exit', function(){
+		console.log("***********\nDONE\n***********")
+	});
 }
 x();
