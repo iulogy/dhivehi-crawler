9c25f5a9066ed31acacbb7f2c05888020a27562b
iuthisaam <iuthisaam@isa0004.(none)>
2012-11-29 20:53:59 +0500

    changed manual lookup to links found within the site

--- crawler.js
+++ crawler.js
@@ -1,10 +1,11 @@
 var spawn = require('child_process').spawn;
 var arg = require('optimist').argv;
 
-setInterval(x, 1000 * 60 * (arg.interval || 2));
+//setInterval(x, 1000 * 60 * (arg.interval || 2));
+setInterval(x, 15000);
 
 function x (){
-	app = spawn('node', ['app.js', '--limit', arg.limit || 3]);
+	app = spawn('node', ['app.js', '--limit', arg.limit || 3, '--sites', 'mvexposed']);
 	app.stdout.setEncoding('utf8');
 	app.stdout.on('data', function(data){
 		console.log(data);
