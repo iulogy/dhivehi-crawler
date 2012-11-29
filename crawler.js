3e2645179df1d2fbbfe9be236351557ece03c46f
iuthisaam <iuthisaam@isa0004.(none)>
2012-11-29 21:12:06 +0500

    reset crawler

--- crawler.js
+++ crawler.js
@@ -1,11 +1,10 @@
 var spawn = require('child_process').spawn;
 var arg = require('optimist').argv;
 
-//setInterval(x, 1000 * 60 * (arg.interval || 2));
-setInterval(x, 15000);
+setInterval(x, 1000 * 60 * (arg.interval || 2));
 
 function x (){
-	app = spawn('node', ['app.js', '--limit', arg.limit || 3, '--sites', 'mvexposed']);
+	app = spawn('node', ['app.js', '--limit', arg.limit || 3]);
 	app.stdout.setEncoding('utf8');
 	app.stdout.on('data', function(data){
 		console.log(data);
