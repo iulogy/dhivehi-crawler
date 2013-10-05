e697233e27998ba078cdf1993bf1ff111c087d11
demon <maldivianist@gmail.com>
2013-10-05 15:42:26 +0500

    added more sources, save to db

--- app.js
+++ app.js
@@ -1,36 +1,77 @@
 //clear screen
 //process.stdout.write ('\u001B[2J\u001B[0;0f')
 
-var util = require('util');
-var arg = require('optimist').argv;
-var Sites = require('./lib');
 
+var events = require('events').EventEmitter;
+var async = require('async')
+var _ = require('underscore');
+var sites = require('./sources.json');
+var Scraper = require('./lib');
+var mongoose = require('mongoose');
 
-var site = new Sites.site();
+var db = mongoose.connect("mongodb://127.0.0.1/scrapes");
 
-//process commands, sync commands, exit on complete
-if(arg.reset){
-	Sites.reset();
-	process.exit();
+var page_schema = mongoose.Schema({
+	_id:'ObjectId',
+	versions:'array'
+},{strict:false});
+var Page = mongoose.model('pages', page_schema);
+
+function fetch(){
+	var sources = _.keys(sites);
+	sources = sources.reverse();
+	console.log(sources)
+	async.eachLimit(
+		sources,
+		4,
+		function(item, callback){
+			var scraper = new Scraper.Scraper(sites[item]);
+			scraper.scrape(function(err, data){
+				callback(null, data);
+			});
+			scraper.on("new item", function(data){
+				Page
+				.findOne({url:data.url})
+				.lean()
+				.exec(function(err, page){
+					if(!page){
+						return new Page(data).save(function(err, p){
+							console.log('saved new page - ' + data.url);
+						});
+					}
+					if(page._resHash != data._resHash){
+						console.log('duplicate page - ' + data.url );
+					}else{
+						var pid = new String(page._id).toString();
+						delete page.versions;
+						delete page._id
+						Page.update({_id:new mongoose.Types.ObjectId(page.pid)}, {$push:{versions:page}}, function(err,p){
+							console.log(p);
+							if(p == 1){
+								Page.update({_id:pid},{$set:page}, function(err, pa){
+									if(err) throw err;
+									console.log('updated - ' + data.url);
+								});
+							}
+						});
+
+					}
+				});
+			});
+			
+		},e
+	)	
+	function e(err, res){
+		var data = {};
+		res.forEach(function(e){
+			for(var i in e){
+				data[i] = e[i];
+			}
+		})
+		
+		//expose data
+		self.emit('fetch', data);
+	}
 }
 
-
-if(arg.sites){
-	site.sources = typeof arg.sites == 'object' ? arg.sites : new Array(arg.sites);
-}
-site.requestLimit = arg.limit +1 || 4;
-site.on('error', function(err){
-	console.log(err);
-})
-
-site.on('fetch', function(data){
-	
-	//[data] contains scraped batch
-	// do something with data 
-	
-	
-	//     ...
-	
-})
-
-site.fetch();
+fetch();
